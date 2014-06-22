310cc4a336bb352d6eea80f402b71112254622a7
Andy Sipe <ajs.general@gmail.com>
2014-06-22 11:02:41 -0400

    added a couple of non string properties

--- src/Samples/Fitter.Sample/Program.cs
+++ src/Samples/Fitter.Sample/Program.cs
@@ -5,11 +5,13 @@ namespace Fitter.Sample {
   internal class Program {
     private static void Main(string[] args) {
       var spec = new {
-                       Root = @"z:\project1",
+                       Root = @"z:\project1\<version>",
                        Source = @"<root>\source",
                        Debug = @"<root>\debug",
-                       Temp = @"<working>\temp",
-                       Working = @"<debug>\working"
+                       Temp = @"<working>\temp\<cycle>",
+                       Working = @"<debug>\working",
+                       Cycle = 1,
+                       Version = new Version(10, 20)
                      };
       var paths = new Builder().Build(spec);
 
@@ -18,6 +20,8 @@ namespace Fitter.Sample {
       Console.WriteLine("paths.Debug: {0}", paths.Debug);
       Console.WriteLine("paths.Working: {0}", paths.Working);
       Console.WriteLine("paths.Temp: {0}", paths.Temp);
+      Console.WriteLine("paths.Cycle: {0}", paths.Cycle);
+      Console.WriteLine("paths.Version: {0}", paths.Version);
     }
   }
 }
